e4794ba2134ca325074d2444d23948177fefafb3
Mahmoud Taha Abdelaal <mahmouddttahaa@gmail.com>
2023-05-09 20:56:35 +0300

    server-side login page completed

---
+++ login.php
@@ -0,0 +1,101 @@
+<?php
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+  $mysqli = require __DIR__ . "/database.php";
+
+  $sql = sprintf(
+    "SELECT * FROM user
+                    WHERE email = '%s'",
+    $mysqli->real_escape_string($_POST["email"])
+  );
+
+  $result = $mysqli->query($sql);
+
+  $user = $result->fetch_assoc();
+
+  if ($user) {
+
+    if (password_verify($_POST["password"], $user["password_hash"])) {
+
+      session_start();
+      session_regenerate_id();
+      $_SESSION["user_id"] = $user["id"];
+
+      header("Location: home.html");
+      exit;
+    }
+  }
+
+  $is_invalid = true;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+
+<body>
+    <header>
+        <div>
+            <a href="./index.html">
+                <h1 class="logo" style="text-transform: uppercase;">Watch<span>topia</span></h1>
+            </a>
+        </div>
+    </header>
+
+    <div class="wrapper">
+        <div class="form-box login">
+            <h2  id="login">Login</h2>
+            <?php if ($is_invalid): ?>
+              <div style="color: red; margin:20px 0px 30px 0px;"><strong>Invalid Login</strong></div>
+            <?php endif; ?>
+            <form action="./login.php" method="post">
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" id="email" name="email" value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                    <label for="email">Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password" id="password" name="password">
+                    <label for="password">Password</label>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">
+                        Remember me</label>
+                    <a href="#">Forgot Password</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p>Don't have an account?
+                        <a href="signup.html" class="register-link">
+                            Register</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
+
